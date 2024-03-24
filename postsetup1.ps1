9d147773dbaa1b0acff51fd70f2c7b87db4b91cb
Flamelord626 <griffin1598159889@gmail.com>
2024-03-24 15:45:16 +0000

    added the main files

---
+++ postsetup1.ps1
@@ -0,0 +1,19 @@
+# winget installation script. Mainly intended for LTSC, since that doesn't come with it preinstalled OOTB.
+Import-Module Appx
+
+$elevated = ([System.Security.Principal.WindowsIdentity]::GetCurrent()).groups -match 'S-1-5-32-544'
+if (-not $elevated) {
+	Write-Host "This script requires administrative privileges. Please run it again in an elevated command prompt." -ForegroundColor Red
+	exit
+}
+
+$progressPreference = 'silentlyContinue'
+Write-Information "Downloading and installing winget and its dependencies..."
+Invoke-WebRequest -Uri https://github.com/microsoft/winget-cli/releases/download/v1.6.3133/b6e881d14bc943268a82d474bf7d15af_License1.xml -OutFile b6e881d14bc943268a82d474bf7d15af_License1.xml
+Invoke-WebRequest -Uri https://aka.ms/getwinget -OutFile Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
+Invoke-WebRequest -Uri https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx -OutFile Microsoft.VCLibs.x64.14.00.Desktop.appx
+# if it's hacky and you know it clap your hands!
+Invoke-WebRequest -Uri "https://github.com/microsoft/terminal/releases/download/v1.19.10302.0/Microsoft.WindowsTerminal_1.19.10302.0_8wekyb3d8bbwe.msixbundle_Windows10_PreinstallKit.zip" -OutFile ".\Windows10_PreinstallKit.zip"; Expand-Archive -Path ".\Windows10_PreinstallKit.zip" -DestinationPath ".\Windows10_PreinstallKit" -Force; Move-Item -Path ".\Windows10_PreinstallKit\Microsoft.UI.Xaml.2.8_8.2310.30001.0_x64__8wekyb3d8bbwe.appx" -Destination .; Remove-Item -Path ".\Windows10_PreinstallKit.zip" -Force; Remove-Item -Path ".\Windows10_PreinstallKit" -Recurse -Force
+Add-AppxPackage -Path Microsoft.UI.Xaml.2.8_8.2310.30001.0_x64__8wekyb3d8bbwe.appx
+Add-AppxPackage Microsoft.VCLibs.x64.14.00.Desktop.appx
+Add-AppxProvisionedPackage -Online -PackagePath Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle -LicensePath .\b6e881d14bc943268a82d474bf7d15af_License1.xml -Verbose
---
+++ postsetup2.ps1
@@ -0,0 +1,40 @@
+# flame's Windows 10 post-install script.
+
+# 9p7knl5rwt25 = sysinternals, 9N0DX20HK701 = windows terminal
+$wingetPackages = "Microsoft.PowerToys", "Discord.Discord", "Google.Chrome", "9p7knl5rwt25", "ShareX.ShareX", "9N0DX20HK701", "VSCodium.VSCodium", "Guru3D.Afterburner", "Oracle.VirtualBox", "Librewolf.Librewolf", "RARLab.WinRAR", "Valve.Steam", "CrystalDewWorld.CrystalDiskInfo", "RealVNC.VNCViewer", "Git.Git", "Notepad++.Notepad++", "Python.Python.3.11", "Microsoft.DotNet.SDK.8", "EpicGames.EpicGamesLauncher", "dotPDNLLC.paintdotnet", "OBSProject.OBSStudio", "Spotify.Spotify", "Nvidia.GeForceExperience", "qBittorrent.qBittorrent", "Microsoft.PowerShell", "StartIsBack.StartIsBack", "Mojang.MinecraftLauncher", "CodecGuide.KLiteCodecPack.Mega", "yt-dlp.yt-dlp", "cURL.cURL", "GNU.Wget2"
+$chocolateyPackages = "tenacity", "photogimp", "handbreak", "inkscape", "blender", "winscp", "etcher", "imgburn", "lghub", "razer-synapse-3", "teracopy", "spotify", "handbrake"
+
+$elevated = ([System.Security.Principal.WindowsIdentity]::GetCurrent()).groups -match 'S-1-5-32-544'
+if (-not $elevated) {
+	Write-Host "This script requires administrative privileges. Please run it again in an elevated command prompt." -ForegroundColor Red
+	exit
+}
+
+Write-Host "The following packages will be installed:"
+foreach ($package in $wingetPackages) {
+	Write-Host "Winget: $package" -ForegroundColor Yellow
+}
+foreach ($package in $chocolateyPackages) {
+	Write-Host "Chocolatey: $package" -ForegroundColor Yellow
+}
+
+Read-Host -Prompt "Press any key to continue"
+
+Write-Host "Installing Chocolatey..."
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+choco feature enable -n allowGlobalConfirmation
+
+foreach ($package in $wingetPackages) {
+	winget install $package --accept-package-agreements
+}
+foreach ($package in $chocolateyPackages) {
+	choco install $package -y # "-y" probably isn't needed, but oh well.
+}
+
+iwr -useb https://raw.githubusercontent.com/spicetify/spicetify-cli/master/install.ps1 | iex
+
+[console]::beep(500,500)		# Beep when complete; a self reminder
+[console]::beep(500,500)
+[console]::beep(500,500)
+
+Read-Host -Prompt "Done, press any key to exit"
